6bb1c2656c5eb85e34a30e6b57e8f84d85288d37
Danielle Mcloughlin <184102558+daniellem62@users.noreply.github.com>
2024-12-03 12:32:39 +0000

    Wrote 4 questions

--- questions.js
+++ questions.js
@@ -2,6 +2,11 @@
 
 const questions = [
   // Add your questions here
+  // 1. What is the cat in Scratch? Sprite, Block, Variable, Avatar
+  // 2. What does git checkout do? Navigate to a different branch, Creates a branch, Deletes a branch, Merges branches
+  // 3. Chris video
+  // 4. Which of these isn't a Disney Ideation room? Dream, Realist, Critic, Creative
+  // 5. 
 ];
 
 // Don't worry about this, we're just exporting the questions
